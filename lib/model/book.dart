b80914594f027050446869b1af61d148b5583b8d
Octagon6257 <l.mazzoli7@campus.uniurb.it>
2025-04-30 21:07:54 +0200

    Added serialization to handle information in book.dart

--- lib/model/book.dart
+++ lib/model/book.dart
@@ -3,4 +3,15 @@ class Book {
   final String author;
 
   Book({required this.title, required this.author});
+
+  Map<String, dynamic> toMap() {
+    return {'title': title, 'author': author};
+  }
+
+  factory Book.fromMap(Map<String, dynamic> map) {
+    return Book(
+      title: map['title'] as String,
+      author: map['author'] as String,
+    );
+  }
 }
