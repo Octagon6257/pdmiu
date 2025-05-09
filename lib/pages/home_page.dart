dec1a86776f60bdead2dd35e6a66f7963bcf61d2
Octagon6257 <l.mazzoli7@campus.uniurb.it>
2025-05-09 08:54:17 +0200

    Created basic HomePage

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Virtual Bookshelf')),
+      body: const Center(child: Text('Pages coming soon')),
+    );
+  }
+}
