52b5e1cc049a022bae00ef9a568ac5e9ea642e90
Octagon6257 <l.mazzoli7@campus.uniurb.it>
2025-05-12 20:29:28 +0200

    Added bottom navigation bar to home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utils/responsive.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +9,39 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final horizontalPadding = Responsive.horizontalPadding(context);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Virtual Bookshelf')),
-      body: const Center(child: Text('Pages coming soon')),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: horizontalPadding),
+        child: Center(child: Text('Page ${_selectedIndex + 1} content coming soon')),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Bookshelf',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+        ],
+      ),
     );
   }
 }
