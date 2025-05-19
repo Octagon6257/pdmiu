6ec5d91ea77d1d08ca3865b0406b24aef645fdd1
Octagon6257 <l.mazzoli7@campus.uniurb.it>
2025-05-19 17:12:53 +0200

    Created search_page.dart skeleton

---
+++ lib/pages/search_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  SearchPageState createState() => SearchPageState();
+}
+
+class SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Search functionality coming soon'));
+  }
+}
