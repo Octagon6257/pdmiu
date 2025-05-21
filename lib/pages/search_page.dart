a9abad5906ca8d21956337c882fe76c61c9f06f8
Octagon6257 <l.mazzoli7@campus.uniurb.it>
2025-05-21 17:10:19 +0200

    Added input field to search_page.dart

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -8,8 +8,28 @@ class SearchPage extends StatefulWidget {
 }
 
 class SearchPageState extends State<SearchPage> {
+  final TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Search functionality coming soon'));
+    return Column(
+      children: [
+        TextField(
+          controller: _controller,
+          decoration: InputDecoration(
+            labelText: 'Search books',
+            suffixIcon: IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                // Search functionality to be implemented
+              },
+            ),
+          ),
+        ),
+        const Expanded(
+          child: Center(child: Text('Enter a search term to find books')),
+        ),
+      ],
+    );
   }
 }
