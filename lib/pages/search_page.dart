59f503976ffc21c3c1b38ace364e3350b8113886
Octagon6257 <l.mazzoli7@campus.uniurb.it>
2025-05-26 18:45:53 +0200

    Completed search_page.dart

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/book.dart';
+import '../providers/book_provider.dart';
+import '../services/open_library_service.dart';
 
 class SearchPage extends StatefulWidget {
+
   const SearchPage({super.key});
 
   @override
@@ -9,9 +14,34 @@ class SearchPage extends StatefulWidget {
 
 class SearchPageState extends State<SearchPage> {
   final TextEditingController _controller = TextEditingController();
+  List<Book> _searchResults = [];
+  bool _isLoading = false;
+
+  void _searchBooks() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      final results = await OpenLibraryService.searchBooks(_controller.text);
+      if (mounted) {
+        setState(() {
+          _searchResults = results;
+        });
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    } finally {
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    final booksProvider = Provider.of<BooksProvider>(context, listen: false);
     return Column(
       children: [
         TextField(
@@ -20,14 +50,37 @@ class SearchPageState extends State<SearchPage> {
             labelText: 'Search books',
             suffixIcon: IconButton(
               icon: const Icon(Icons.search),
-              onPressed: () {
-                // Search functionality to be implemented
-              },
+              onPressed: _searchBooks,
             ),
           ),
+          onSubmitted: (_) => _searchBooks(),
         ),
-        const Expanded(
-          child: Center(child: Text('Enter a search term to find books')),
+        const SizedBox(height: 10),
+        _isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Expanded(
+          child: ListView.builder(
+            itemCount: _searchResults.length,
+            itemBuilder: (context, index) {
+              final book = _searchResults[index];
+              return ListTile(
+                title: Text(book.title),
+                subtitle: Text(book.author),
+                trailing: IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () async {
+                    final scaffoldMessenger = ScaffoldMessenger.of(context);
+                    await booksProvider.addBook(book);
+                    if (mounted) {
+                      scaffoldMessenger.showSnackBar(
+                        const SnackBar(content: Text('Book added!')),
+                      );
+                    }
+                  },
+                ),
+              );
+            },
+          ),
         ),
       ],
     );
